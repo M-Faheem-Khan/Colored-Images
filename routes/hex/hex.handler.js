272040c1c3ae3d6cc443d9e7d993a30efd93e881
Faheem Khan <faheem5948@gmail.com>
2020-07-30 02:29:51 -0400

    added size parameters

--- routes/hex/hex.handler.js
+++ routes/hex/hex.handler.js
@@ -1,6 +1,7 @@
 // importing common functions
 const { GenerateImage, GetRandomHexColor } = require("../../lib/common");
 
+
 /**
  * defaultRouteHandler
  * Route: /
@@ -12,15 +13,38 @@ const defaultRouteHandler = (request, response) => {
     response.status(200).send("HEX default route")
 }
 
+
 /**
  * ProcessHexColor
- * Route: /hex/:color
+ * Route: /hex/:size/:color
  * Method: GET
  * Description: Returns an image with the given color(hex value)
  */
 const ProcessHexColor = (request, response) => {
     const color = "#" + request.params.color; // getting color
-    var imageData = GenerateImage(color);
+    var size = request.params.size; // getting size
+
+    var imageData;
+
+    if (!color) {
+        response.status(400).send({
+            "error": "Invalid Color",
+            "message": "No Color was sent, '/hex/size[optional]/hex{no # in front}",
+            "example": "/hex/1000x1200/EF12EF"
+        });
+    }
+
+    // Checking if size is valid
+    if (!size || size.length < 3) {
+        imageData = GenerateImage(color);
+    } else {
+        size = size.split("x"); // splitting the size WidthxHeight
+        var width = parseInt(size[0]);
+        var height = parseInt(size[1]);
+        imageData = GenerateImage(color, height, width);
+    }
+
+
     // removing the image identifier to create buffer
     imageData = imageData.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
     var image = Buffer.from(imageData, 'base64'); // creating base64 image buffer
@@ -41,7 +65,21 @@ const ProcessHexColor = (request, response) => {
  */
 const RandomHexColor = (request, response) => {
     const color = GetRandomHexColor(); // generating random color
-    var imageData = GenerateImage(color);
+    var size = request.params.size; // getting size
+    var imageData; // holds the image data base64 encoded
+
+    console.log(`Color: ${color}`);
+    
+    // Checking if size is valid
+    if (!size || size.length < 3) {
+        imageData = GenerateImage(color);
+    } else {
+        size = size.split("x"); // splitting the size WidthxHeight
+        var width = parseInt(size[0]);
+        var height = parseInt(size[1]);
+        imageData = GenerateImage(color, height, width);
+    }
+
     // removing the image identifier to create buffer
     imageData = imageData.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
     var image = Buffer.from(imageData, 'base64'); // creating base64 image buffer
@@ -51,6 +89,7 @@ const RandomHexColor = (request, response) => {
         'Content-Type': 'image/png',
         'Content-Length': image.length
     });
+
     // Sending the Actual Image
     response.end(image);
 }
